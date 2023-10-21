893a71e540f5d3eea47aa303bcd5104337abbb79
Raihan Rabbani <raihanrabbani0101@gmail.com>
2023-10-21 07:27:46 +0700

    customize login view

--- lib/view/login_view.dart
+++ lib/view/login_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:rpedia/widget/textfield_widget.dart';
 
 class LoginView extends StatelessWidget {
@@ -9,79 +10,95 @@ class LoginView extends StatelessWidget {
   Widget build(BuildContext context) {
     TextTheme textTheme = Theme.of(context).textTheme;
     TextStyle loginCaption = textTheme.labelMedium!.copyWith(fontSize: 10);
-    ColorScheme colorScheme = Theme.of(context).colorScheme;
     return Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0, 16, 0, 12),
-            child: Image.asset(
-              'images/rpedia-logos/logo-no-background.png',
-              semanticLabel: 'Rpedia Logo',
-            ),
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 16, 0, 12),
+          child: Image.asset(
+            'images/rpedia-logos/logo-no-background.png',
+            semanticLabel: 'Rpedia Logo',
           ),
-          const Padding(
-            padding: EdgeInsets.only(bottom: 16),
-            child: Column(
-              children: [
-                TextFieldWidget(
-                  preIcon: Icons.people_alt,
-                  placeholder: 'Username / Email',
-
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                TextFieldWidget(
-                  preIcon: Icons.lock,
-                  suIcon: Icons.remove_red_eye,
-                  placeholder: 'Password',
-
-                ),
-              ],
-            ),
+        ),
+        const Padding(
+          padding: EdgeInsets.only(bottom: 8),
+          child: Column(
+            children: [
+              TextFieldWidget(
+                preIcon: Icons.people_alt,
+                placeholder: 'Username / Email',
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              TextFieldWidget(
+                preIcon: Icons.lock,
+                suIcon: Icon(Icons.remove_red_eye),
+                placeholder: 'Password',
+                obscureText: true,
+              ),
+            ],
           ),
-          SizedBox(
-              width: 200,
-              height: 40,
-              child:
-                  FilledButton(onPressed: () {}, child: const Text('Login'))),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8),
-            child: Column(
-              children: [
-                RichText(
-                  text: TextSpan(
-                    style: loginCaption,
-                    children: [
-                      const TextSpan(
-                        text: "Doesn't have an account? ",
-                      ),
-                      TextSpan(
-                        text: 'Register',
-                        style: const TextStyle(color: Colors.blue),
-                        recognizer: TapGestureRecognizer()..onTap = () {},
-                      ),
-                    ],
-                  ),
-                ),
-                Text(
+        ),
+        SizedBox(
+            width: 200,
+            height: 40,
+            child: FilledButton(onPressed: () {}, child: const Text('Login'))),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          child: Row(
+            children: [
+              const Expanded(
+                  child: Divider(
+                thickness: 1,
+              )),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                child: Text(
                   "Or",
                   style: loginCaption,
                 ),
-              ],
-            ),
-          ),
-          SizedBox(
-              height: 40,
-              child: FilledButton.tonalIcon(
-                onPressed: () {},
-                icon: Image.asset('images/icons/google-icon.png'),
-                label: Text(
-                  'Continue with google',
-                  style: textTheme.labelMedium,
-                ),
+              ),
+              const Expanded(
+                  child: Divider(
+                thickness: 1,
               )),
-        ],
-      );
+            ],
+          ),
+        ),
+        SizedBox(
+            height: 40,
+            child: FilledButton.tonalIcon(
+              onPressed: () {},
+              icon: Image.asset('images/icons/google-icon.png'),
+              label: Text(
+                'Continue with google',
+                style: textTheme.labelMedium,
+              ),
+            )),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8),
+          child: Column(
+            children: [
+              RichText(
+                text: TextSpan(
+                  style: loginCaption,
+                  children: [
+                    const TextSpan(
+                      text: "Doesn't have an account? ",
+                    ),
+                    TextSpan(
+                      text: 'Register',
+                      style: const TextStyle(color: Colors.blue),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () => context.go('/register'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
